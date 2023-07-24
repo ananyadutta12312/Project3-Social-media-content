c83c298cfb0c389558dc6c9400e2ef0ad2f0c662
ANANYA DUTTA <91021007+ananyadutta12312@users.noreply.github.com>
2023-07-25 01:20:10 +0530

    Add files via upload

--- Instagram_download.py
+++ Instagram_download.py
@@ -7,7 +7,7 @@ user = input("Enter username: ")
 #check if account is private
 profile = instaloader.Profile.from_username(ig.context,user)
 private = profile.is_private
-print("Is account private:")
+print("Is account private:",private)
 
 # #download profile pic
 ig.download_profile(user,profile_pic_only=True)
@@ -15,21 +15,21 @@ ig.download_profile(user,profile_pic_only=True)
 print("For more account details please login")
 print("Do you want to login: y/n")
 log = input()
-if log == "y"
+if log == "y":
     username = input("Enter username: ")
     password = input("Enter password: ")
     ig.login(username,password)
 
     # download all tagged post
-    ig.download_profile(user,download_tagged = True)
+    #ig.download_profile(user,download_tagged = True)
 
     # download all stories
-    ig.download_profile(user,download_stories=True)
+    #ig.download_profile(user,download_stories=True)
 
     # download all post
     ig.download_profile(user,profile_pic_only=False)
 
-    print list of followers
+    #print list of followers
     followers = profile.get_followers()
     for follower in followers:
         print(follower)
